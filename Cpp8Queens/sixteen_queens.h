e9c6110b8c4232ab7deae52836c70f762b02844d
Pablo-Aliskevicius <Pablo-Aliskevicius@users.noreply.github.com>
2022-09-17 20:30:12 +0300

    Oopsie
    Header for the 16x16 version (using AVX2 instructions)

---
+++ Cpp8Queens/sixteen_queens.h
@@ -0,0 +1,12 @@
+// queens.h
+// Solution for 8x8 board. Same as any board I've ever seen
+
+
+namespace qns16
+{
+    void solve();
+    void set_verbose(bool new_val);
+    void set_short(int trials);
+    void test();
+    void set_board_size(int size);
+}
